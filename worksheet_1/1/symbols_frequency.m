7b582568deac113e9307a7dba936d81e33cc5e35
tmartins1 <tiagomartins282@gmail.com>
2017-10-06 21:47:26 +0100

    changed code appearance

--- worksheet_1/1/symbols_frequency.m
+++ worksheet_1/1/symbols_frequency.m
@@ -3,8 +3,6 @@ function symbols_frequency(source, alphabet)
   frequency = arrayfun(@(x) sum(histc(source, x)), alphabet);
   bar(frequency);
   set(gca, 'XTickLabel', cellstr(num2str(alphabet'))', 'XTick', 1:numel(alphabet));
-  xtickangle(90);
-  xlabel('alphabet');
-  ylabel('frequency');
+  xtickangle(90), xlabel('alphabet'), ylabel('frequency');
 
 end
